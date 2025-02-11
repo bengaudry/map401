d03615c76e9f4b86c7821aaf11e459a6ba228bea
Gaudry Ben <bengaudry@outlook.fr>
2025-02-11 13:06:46 +0100

    Tache 5 bis

--- test_contours_multiples.c
+++ test_contours_multiples.c
@@ -29,18 +29,27 @@ int main (int argc, char **argv) {
 
     I = lire_fichier_image(argv[1]);
 
-    printf("Largeur : %d\n", largeur_image(I));
-    printf("Hauteur : %d\n", hauteur_image(I));
-
+    // Création d'une séquence de contours
     seq_contours = initialiser_sequence_contours();
     parcourir_contours(I, seq_contours);
     printf("%d contours detectés\n", seq_contours->taille);
 
+    // Parcours et affichage des contours de l'image
+    int total_points = 0;
     cel_seq_contours = seq_contours->first;
     while (cel_seq_contours != NULL) {
         ecrire_contour(cel_seq_contours->value);
+        printf("%d segments\n\n", cel_seq_contours->value.taille);
+        total_points += cel_seq_contours->value.taille;
         cel_seq_contours = cel_seq_contours->suiv;
     }
+
+    // Affichage des statistiques
+    printf("%d contours\n", seq_contours->taille);
+    printf("Total points: %d\n", total_points);
+    printf("Total segments: %d\n", total_points - seq_contours->taille);
+
+    // Export au format eps
     exporter_image_eps(I, seq_contours, extraire_nom_fichier(argv[1]), argv[2] == NULL ? "fill" : argv[2]);
 
     return 0;
